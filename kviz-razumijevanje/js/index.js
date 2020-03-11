6a62223ebf3191acfc02e2a1e9ccd1fddc22c0d1
josip <jomihx@gmail.com>
2020-03-11 14:35:39 +0100

    ispravci

--- kviz-razumijevanje/js/index.js
+++ kviz-razumijevanje/js/index.js
@@ -123,25 +123,7 @@ $(document).ready(function () {
             }
         })
     }
-    $(".fa-info-circle").on('click', function () {
-        prekidac=0
-        swal({
-            title: "",
-            html: "<p style='text-align:justify; font-size:18px'>" + $(".prica").text().substring(0,$(".prica").text().length-7) + "</p>",
-            showCloseButton: true,
-            confirmButtonText: ' dalje',
-            backdrop: false,
-            allowOutsideClick: false,
-            allowEscapeKey: false
-        });
-
-        $(".swal2-confirm").click(function () {
-            prekidac=1
-        })
-        $(".swal2-close").click(function () {
-            prekidac=1
-        })
-    })
+  
     // Start the quiz
     newQuiz = function () {
         prekidac = 1;
@@ -205,17 +187,24 @@ $(document).ready(function () {
         }
 
         
-    $(".info").on('click', function () {
+    $(".info").unbind("click").click(function() {
         prekidac=0
         swal({
             title: "",
             html: "<p style='text-align:justify; font-size:18px'>" + $(".prica").text().substring(0,$(".prica").text().length-7) + "</p>",
-            showCloseButton: true,
-            confirmButtonText: ' dalje',
+            showCloseButton: false,
+            confirmButtonText: 'ok',
             backdrop: false,
             allowOutsideClick: false,
             allowEscapeKey: false
         });
+
+        $(".swal2-confirm").unbind("click").click(function() {
+            prekidac=1
+        })
+        $(".swal2-close").unbind("click").click(function() {
+            prekidac=1
+        })
     })
         question.html(quiz[questionCounter].question)
         $(".questions-page__question").prepend("<span style='font-size: 1.3rem;'>" + (questionCounter + 1) + "/" + quiz.length + ".</span> <br>")
@@ -318,20 +307,19 @@ $(document).ready(function () {
 
 
     // Clicking on story button:
-    storyBtn.on('click', function () {
+    storyBtn.unbind("click").click(function() {
         $(".prica").show()
         $(".opis").hide()
         $(".story-page__btn").hide()
         $(".init-page__title").text($(this).text())
         if ($(this).hasClass( "korištenje-automobila" ))
         {
-            $(".prica").html('<img src="slike/dijeljenje-auta.png"  class="senka"><p><span class="soundcite" data-url="audio/auto1.mp3" data-plays="1" dir=ltr>Najčešće jednim automobilom putuje jedna, ponekad dvije osobe. Zbog toga nastaju velike gužve na gradskim cestama. To je i skupo. Ako odlučimo na posao putovati sa susjedom iii kolegom, jedan dan putovat ćemo svojim automobilom, drugi dan njegovim. Tako štedimo novac i automobil, a imanje se umaramo.</span><br><br><span class="soundcite" data-url="audio/auto2.mp3" data-plays="1" dir=ltr>U mnogim europskim gradovima postoje organizacije u kojima nekoliko ljudi ima jedan automobil. Na vrijeme morate rezervirati automobil. Imate ga kada vam je potreban, a u ostalo vrijeme koristi ga netko drugi. Zajedno plaćate osiguranje, održavanje i registriranje automobila. Manje automobila, manje gužve na cestama, više parkirnih mjesta, manje novca za benzin i čistiji zrak!</span></p> <button class="init-page__btn btn koristenje-automobila">pokreni</button>')
+            $(".prica").html('<img src="slike/dijeljenje-auta.png"  class="senka"><p><span  data-url="audio/auto1.mp3" data-plays="1" dir=ltr>Najčešće jednim automobilom putuje jedna, ponekad dvije osobe. Zbog toga nastaju velike gužve na gradskim cestama. To je i skupo. Ako odlučimo na posao putovati sa susjedom iii kolegom, jedan dan putovat ćemo svojim automobilom, drugi dan njegovim. Tako štedimo novac i automobil, a imanje se umaramo.</span><br><br><span  data-url="audio/auto2.mp3" data-plays="1" dir=ltr>U mnogim europskim gradovima postoje organizacije u kojima nekoliko ljudi ima jedan automobil. Na vrijeme morate rezervirati automobil. Imate ga kada vam je potreban, a u ostalo vrijeme koristi ga netko drugi. Zajedno plaćate osiguranje, održavanje i registriranje automobila. Manje automobila, manje gužve na cestama, više parkirnih mjesta, manje novca za benzin i čistiji zrak!</span></p> <button class="init-page__btn btn koristenje-automobila">pokreni</button>')
         }
         else{
-            $(".prica").html('<img src="slike/tramvaj.jpg"  class="senka"><p><span class="soundcite" data-url="audio/tram1.mp3" data-plays="1" dir=ltr>Zagrepčani su 1891. godine dobili prvi tramvaj. Vukli su ga konji. Prvi električni tramvaji počeli su voziti 1905. godine. Četiri godine prije, 1901., prvi automobil pojavio se na zagrebačkim ulicama.</span><br><span class="soundcite" data-url="audio/tram2.mp3" data-plays="1" dir=ltr>A u svijetu?</span></br><span class="soundcite" data-url="audio/tram3.mp3" data-plays="1" dir=ltr>Prvi tramvaj postavljen je na tračnice 1832. godine u New Yorku. Također su ga vukli konji. Prvi električni tramvaj na svijetu izgrađen je u tvornici „Werner von Siemens” i pokrenut je 1881. godine u Berlinu, a vozio je maksimalnom brzinom od 20 kilometara na sat.</span></br><span class="soundcite" data-url="audio/tram4.mp3" data-plays="1" dir=ltr>Prvi automobil, marke Mercedes, konstruirao je Gotlieb Daimler 1885. U Francuskoj se prvi automobil pojavio 1890., a u Americi 1892. godine. Proizveo ga je Henry Ford.</span></p><button class="init-page__btn btn prvi-tram">pokreni</button>')
+            $(".prica").html('<img src="slike/tramvaj.jpg"  class="senka"><p><span  data-url="audio/tram1.mp3" data-plays="1" dir=ltr>Zagrepčani su 1891. godine dobili prvi tramvaj. Vukli su ga konji. Prvi električni tramvaji počeli su voziti 1905. godine. Četiri godine prije, 1901., prvi automobil pojavio se na zagrebačkim ulicama.</span><br><span  data-url="audio/tram2.mp3" data-plays="1" dir=ltr>A u svijetu?</span></br><span  data-url="audio/tram3.mp3" data-plays="1" dir=ltr>Prvi tramvaj postavljen je na tračnice 1832. godine u New Yorku. Također su ga vukli konji. Prvi električni tramvaj na svijetu izgrađen je u tvornici „Werner von Siemens” i pokrenut je 1881. godine u Berlinu, a vozio je maksimalnom brzinom od 20 kilometara na sat.</span></br><span  data-url="audio/tram4.mp3" data-plays="1" dir=ltr>Prvi automobil, marke Mercedes, konstruirao je Gotlieb Daimler 1885. U Francuskoj se prvi automobil pojavio 1890., a u Americi 1892. godine. Proizveo ga je Henry Ford.</span></p><button class="init-page__btn btn prvi-tram">pokreni</button>')
         }
 
-        $.getScript('js/soundcite.min.js');
 
         $(".init-page__btn").click(function(){
             if($(this).hasClass("koristenje-automobila")){
@@ -360,7 +348,7 @@ $(document).ready(function () {
     /* --- PAGE 2/3 --- */
 
     // Clicking on an answer:
-    answerDiv.on('click', function () {
+    answerDiv.unbind("click").click(function() {
         // Make the submit button visible
         // Remove pointer from any answer that already has it
         deselectAnswer();
@@ -375,7 +363,6 @@ $(document).ready(function () {
 
 
     function odgovor() {
-        $(".questions-page__answer-span").html("")
         vrijeme = parseInt($("#pageBeginCountdownText").text())
         bodovi += vrijeme
         prekidac = 0;
@@ -396,7 +383,7 @@ $(document).ready(function () {
             swal({
                 title: "Isteklo je vrijeme.",
                 html: "<p style='text-align:center'><strong>Trebali ste odgovoriti: " + quiz[questionCounter].correctAnswer + "</strong></p><br>" + quiz[questionCounter].napomena,
-                showCloseButton: true,
+                showCloseButton: false,
                 confirmButtonText: ' dalje',
                 backdrop: false,
                 allowOutsideClick: false,
@@ -407,17 +394,13 @@ $(document).ready(function () {
                 "color": "#bb422a",
                 "text-decoration": "underline"
             })
-            $(".swal2-confirm").click(function () {
+            $(".swal2-confirm").unbind("click").click(function() {
                 clearInterval(countdownTimer)
                 if (ide == 1) {
                     ProgressCountdown(30, 'pageBeginCountdown', 'pageBeginCountdownText').then(value => odgovor());
                 }
-            })
-            $(".swal2-close").click(function () {
-                clearInterval(countdownTimer)
-                if (ide == 1) {
-                    ProgressCountdown(30, 'pageBeginCountdown', 'pageBeginCountdownText').then(value => odgovor());
-                }
+                nastavi()
+                return
             })
         } else {
             // Evaluate if the user got the answer right or wrong
@@ -430,22 +413,18 @@ $(document).ready(function () {
                 swal({
                     title: "<span style='color:green'>Točno</span>",
                     html: "+ <span class='zeleno'>" + broj + "</span><br>" + quiz[questionCounter].napomena,
-                    showCloseButton: true,
+                    showCloseButton: false,
                     confirmButtonText: ' dalje',
                     backdrop: false,
                     allowOutsideClick: false,
                     allowEscapeKey: false
                 });
-                $(".swal2-confirm").click(function () {
+                $(".swal2-confirm").unbind("click").click(function() {
                     clearInterval(countdownTimer)
                     if (ide == 1) {
                         ProgressCountdown(30, 'pageBeginCountdown', 'pageBeginCountdownText').then(value => odgovor());
-                    }
-                })
-                $(".swal2-close").click(function () {
-                    clearInterval(countdownTimer)
-                    if (ide == 1) {
-                        ProgressCountdown(30, 'pageBeginCountdown', 'pageBeginCountdownText').then(value => odgovor());
+                        nastavi()
+                        return
                     }
                 })
             } else {
@@ -455,22 +434,18 @@ $(document).ready(function () {
                 swal({
                     title: " <span style='color:#bb422a' >Netočno</span>",
                     html: "<p style='text-align:center'></p><br>" + quiz[questionCounter].napomena,
-                    showCloseButton: true,
+                    showCloseButton: false,
                     confirmButtonText: ' dalje',
                     backdrop: false,
                     allowOutsideClick: false,
                     allowEscapeKey: false
                 });
-                $(".swal2-confirm").click(function () {
+                $(".swal2-confirm").unbind("click").click(function() {
                     clearInterval(countdownTimer)
                     if (ide == 1) {
                         ProgressCountdown(30, 'pageBeginCountdown', 'pageBeginCountdownText').then(value => odgovor());
-                    }
-                })
-                $(".swal2-close").click(function () {
-                    clearInterval(countdownTimer)
-                    if (ide == 1) {
-                        ProgressCountdown(30, 'pageBeginCountdown', 'pageBeginCountdownText').then(value => odgovor());
+                        nastavi()
+                        return
                     }
                 })
                 $(".nadopuni").html(quiz[questionCounter].correctAnswer)
@@ -482,10 +457,12 @@ $(document).ready(function () {
         }
         // Substitute the submit button for the continue button:
         submitBtn.hide(300);
-        nastavi()
+    
     }
     // Clicking on the submit button:
     function nastavi() {
+        $(".questions-page__answer-span").html("")
+        prekidac=1;
         // Increment question number until there are no more questions, then advance to the next page
         if (questionCounter < quiz.length - 1) {
             questionCounter++;
@@ -509,7 +486,7 @@ $(document).ready(function () {
         // Hide the continue button
         continueBtn.hide(300);
         // Enable ability to select an answer
-        answerDiv.on('click', function () {
+        answerDiv.unbind("click").click(function() {
             // Make the submit button visible
             // Remove pointer from any answer that already has it
             deselectAnswer();
@@ -525,7 +502,7 @@ $(document).ready(function () {
     }
 
     // Clicking on the continue button:
-    continueBtn.on('click', function () {
+    continueBtn.unbind("click").click(function() {
 
     });
 
@@ -535,7 +512,7 @@ $(document).ready(function () {
     /* --- PAGE 3/3 --- */
 
     // Clicking on the retake button:
-    retakeBtn.on('click', function () {
+    retakeBtn.unbind("click").click(function() {
         // Go to the first page
         // Start the quiz over
         newQuiz();
